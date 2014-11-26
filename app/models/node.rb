3b69eca717b80e2121fd6bce5dc97628116d791b
doutatsu <doutatsu@me.com>
2014-11-26 16:22:24 +0000

    delete bower_components

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 class Node < ActiveRecord::Base
 	def as_json(options)
-		hash = super.as_json(options) 
+		hash = super.as_json(options)
 		hash['domain'] = PublicSuffix.parse(URI(self.link).host).sld unless self.link.nil?
     hash['edges'] = self.edges.split(",").map(&:to_i) unless self.edges.nil?
 		return hash
