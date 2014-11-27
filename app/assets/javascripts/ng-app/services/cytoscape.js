7797096d9c0f328fda967859fcf06d175ab4494f
Ersel Aker <erselaker@gmail.com>
2014-11-27 19:06:11 +0000

    3rd Party embed function for youtube and soundcloud. This logic should be refactored into a separate factory.

--- app/assets/javascripts/ng-app/services/cytoscape.js
+++ app/assets/javascripts/ng-app/services/cytoscape.js
@@ -1,5 +1,5 @@
-angular.module('NodeZen').factory('cytoscape', ['$q', 'config',
-    function ($q, config) {
+angular.module('NodeZen').factory('cytoscape', ['$q', '$http', 'config',
+    function ($q, $http, config) {
         var nodegraph = {};
         var cytoscapeinstance;
         var nodeCollection = [];
@@ -14,7 +14,7 @@ angular.module('NodeZen').factory('cytoscape', ['$q', 'config',
                     cytoscapeinstance.layout({
                         name: config.layout.name
                     });
-                    cytoscapeinstance.center();
+                    //cytoscapeinstance.center();
                 };
                 deferred.resolve(cytoscapeinstance);
             };
@@ -38,7 +38,10 @@ angular.module('NodeZen').factory('cytoscape', ['$q', 'config',
                         name: nodes[i].title,
                         source: nodes[i].source,
                         target: nodes[i].target,
-                        tooltip: nodes[i].tooltip
+                        tooltip: nodes[i].tooltip,
+                        link: nodes[i].link,
+                        domain: nodes[i].domain,
+                        description: nodes[i].description
                     }
                 });
             };
@@ -86,19 +89,45 @@ angular.module('NodeZen').factory('cytoscape', ['$q', 'config',
                 style: {
                    classes: 'qtip-bootstrap qtip-rounded qtip-shadow',
                     width: 600,
-               }
+                }
             });
+
         };
 
         nodegraph.generateTooltipContent = function (node) {
-            var toolTipContent;
+            var toolTipContent = "";
             //title
-            toolTipContent = '<h1>' + node.name + '</h1><hr>';
+            //toolTipContent = '<h1>' + node.name + '</h1><hr>';
 
             //image
+            /*
             if((node.image || null) !== null){
                 toolTipContent += '<img src="' + node.image + '" height="100px"><hr>';
             }
+            */
+
+            if(node.domain === "youtube") {
+                var videoID;
+                var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+                var match = node.link.match(regExp);
+
+                if (match&&match[7].length==11){
+                    videoID = match[7];
+                }
+
+                toolTipContent += '<iframe id="ytplayer" style="z-index:999999" type="text/html" width="100%" height="270" src="https://www.youtube.com/embed/' + videoID + '?autoplay=1&theme=light" frameborder="0" allowfullscreen>';
+
+            } else if (node.domain === "soundcloud") {
+                var client_ID = '9fe2a2240624e436a99fd049a59af11c';
+                toolTipContent += '<div id="soundcloud-import">SoundCloud player loading.</div>';
+                $http.get('http://api.soundcloud.com/resolve.json?url=' + node.link + '&client_id=' + client_ID) 
+                    .success(function(result) {//returns json, we only need id in this case
+                        $("#soundcloud-import").replaceWith('<iframe width="100%" height="180px" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + result.id +'&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>');
+                    })
+                    .error(function(data, status, headers, config){
+                        $("#soundcloud-import").replaceWith('<div id="soundcloud-import">SoundCloud player failed to load.</div>');
+                    });   
+            } 
 
             //desc
             toolTipContent += '<p>' + node.description + '</p>';
