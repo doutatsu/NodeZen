d8845921952e22dc47c9b5c58b5ec47b6ee656f5
doutatsu <doutatsu@me.com>
2014-11-29 22:13:34 +0000

    trying to get sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,14 @@ NodeZen::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.cache_classes = true
   config.serve_static_assets = true
-  config.assets.compile = true
-  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
