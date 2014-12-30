5e005c406e490a51f6cc7d1089f85e3d82dd143c
Craig Roddy <craigroddy@hotmail.com>
2014-12-30 06:15:42 +0000

    commit starting to refactor the graph directive (labels appear to be displaying twice). Would like to further refactor and then change existing factories (node,d3) into just standard services

--- app/assets/javascripts/ng-app/directives/graph.js
+++ app/assets/javascripts/ng-app/directives/graph.js
@@ -1,10 +1,9 @@
 angular.module('NodeZen')
-  .directive('graph', ['$window', 'd3tip','$filter', function ($window, d3tip, $filter) {
+    .directive('graph', ['$window', 'd3tip', '$filter', 'nodeFactory', 'labelFactory', function ($window, d3tip, $filter, nodeFactory, labelFactory) {
 
       var width;
       var height = 700 - .5;
       var colour = d3.interpolateRgb("#f77", "#77f");
-      var icons_codes = {"video": "\uf04b", "music": "\uf001", "article": "\uf0f6", "website": "\uf0ac"}
 
       return {
         restrict: 'E',
@@ -59,23 +58,12 @@ angular.module('NodeZen')
       	    edges.push({source: sourceNode, target: targetNode, weight : Math.random()});
       	 });
       	}
-      	var labels = [];
-      	var labelsEdges = [];
-      	data.nodes.forEach(function(node){
-        	labels.push({
-			      node : node
-		      });
-      	})
 
-        var i = 0;
-				data.nodes.forEach(function(edge){
-				  labelsEdges.push({
-  				 	source : 0,
-  				 	target : i + 1,
-  				 	weight : 1
-				  })
-				});
-				
+      	// nasty code will be changed to an actual service over factory as more appropiate 
+        labelFactory.initializeLabels(data);
+        var labels = labelFactory.getAllLabels();
+        var labelsEdges = labelFactory.getAllLabelsEdges();
+
         window.force = d3.layout.force()
 					.charge(-3000)
 					.gravity(1)
@@ -164,131 +152,35 @@ angular.module('NodeZen')
               .enter().append("line")
               .attr("class", "link");
 
-            var label = svg.selectAll(".label")
-        			.data(force2.nodes())
-        			.enter().append("g")
-        			.attr("class", "label")
+            var label = labelFactory.styleLabels(svg, force2);
+            var labelLinks = svg.selectAll(".Labellink").data(labelsEdges)
+            var node = nodeFactory.selectAllNodes(svg, force, label, width, height);
 
-            label.append("line")
-              .style("stroke", "black")          // colour the line
-              .style("stroke-width", 20)         // adjust line width
-              .style("stroke-linecap", "round")  // stroke-linecap type
-              .attr("x1", -18)     // x position of the first end of the line
-              .attr("x2", 200)     // x position of the second end of the line
-              .attr("y1", -25)     // x position of the first end of the line
-              .attr("y2", -25);    // x position of the second end of the line
+			var updateLink = function() {
+				this.attr("x1", function(d) {
+					return d.source.x;
+				}).attr("y1", function(d) {
+					return d.source.y;
+				}).attr("x2", function(d) {
+					return d.target.x;
+				}).attr("y2", function(d) {
+					return d.target.y;
+				});
+			}
 
-          	label.append("text")
-          	  .attr("dx", 45)
-              .attr("dy", -20)
-              .attr("pointer-events", "none")
-              .attr("font", "13px open_sansregular")
-              .attr("fill", "white")
-              .text(function (d) {
-                return $filter('limitTo')(d.node.name, 23) + "...";
-              });
+			var updateNode = function() {
+				this.attr("transform", function(d) {
+					return "translate(" + d.x + "," + d.y + ")";
+				});
+			}
 
-              var labelLinks = svg.selectAll(".Labellink").data(labelsEdges)
+			var getMin = function(array){
+			  return Math.min.apply(Math,array);
+			}
 
-              var node = svg.selectAll(".node")
-                .data(force.nodes())
-                .enter().append("g")
-                .attr("class", "node")
-                .attr("cx", -1*(width/2 - 25))
-    			      .attr("cy", function(d, i) { return (i*20-height/7*3 + 20); } )
-                // .on('mouseover', tip.show)
-                .on('click', function(node){
-                  var nodes        = svg.selectAll(".node");      // load all nodes
-                  var rootNode     = nodes[0][nodes[0].length-1]; // store middle node
-                  var rootLabel    = label[0][label[0].length-1]; // store middle node
-                  var selectedNode = this;
-                  var selectedLabel;
-                  // Find right label
-                  label.each(function(d, i) {                            
-                    if (d3.select(selectedNode).datum().id == d.node.id) {
-                      selectedLabel = label[0][d.index];
-                    };
-                  });
-                  // Move chosen node to the middle
-                  d3.select(selectedNode)
-                    .transition()
-                      .duration(750)
-                      .attr("transform", d3.select(rootNode).attr("transform"))
-                    .each("end", function() {
-                      // load node children
-                      scope.$parent.getNodes(node.id);
-                    })
-                  // Move chosen node's label to the middle
-                  d3.select(selectedLabel)
-                    .transition()
-                      .duration(750)
-                      .attr("transform", d3.select(rootLabel).attr("transform"))
-                  // Retract edges
-                  // d3.selectAll(".link")
-                  //   .transition()
-                  //     .duration(750)
-                  //     .style('opacity','0')
-                  // select all the nodes except the node we've chosen
-                  d3.selectAll(nodes[0]).filter(function(d,i) {
-                    return (this !== selectedNode);
-                  })
-                  // Fade them out
-                  .transition()
-                    .duration(750)
-                    .style('opacity','0')
-                  // select all the labels except the node's label we've chosen
-                  d3.selectAll(label[0]).filter(function(d,i) {
-                    return (this !== selectedLabel);
-                  })
-                  // Fade them out
-                  .transition()
-                    .duration(750)
-                    .style('opacity','0');
-                  // Extract edges
-                });
-              node.append("circle")
-                .attr("x", -64)
-                .attr("y", -32)
-                .attr("r", 40)
-                .style("stroke", "gray")
-                .style("stroke-width", 2)
-              // Icons
-              node.append('text')
-                .attr('text-anchor', 'middle')
-                .attr("pointer-events", "none")
-                .attr('dominant-baseline', 'central')
-                .attr('font-family', 'FontAwesome')
-                .attr('font-size', '30px')
-                .attr('fill', 'black')
-                .text(function(d) { 
-                    return icons_codes[d.kind]; 
-                }); 
-             
-				var updateLink = function() {
-					this.attr("x1", function(d) {
-						return d.source.x;
-					}).attr("y1", function(d) {
-						return d.source.y;
-					}).attr("x2", function(d) {
-						return d.target.x;
-					}).attr("y2", function(d) {
-						return d.target.y;
-					});
-				}
-
-				var updateNode = function() {
-					this.attr("transform", function(d) {
-						return "translate(" + d.x + "," + d.y + ")";
-					});
-				}
-
-				var getMin = function(array){
-				  return Math.min.apply(Math,array);
-				}
-
-				var getMax = function(array){
-				  return Math.max.apply(Math,array);
-				}
+			var getMax = function(array){
+			  return Math.max.apply(Math,array);
+			}
 
         force.on("tick", function () {
         	force2.start();
