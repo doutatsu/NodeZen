72d3264012a0bfc5db6c0662ebaa59f4ed83a242
doutatsu <doutatsu@me.com>
2015-01-04 22:05:22 +0000

    dynamic subscribe added

--- app/controllers/subscribe_api_controller.rb
+++ app/controllers/subscribe_api_controller.rb
@@ -2,16 +2,14 @@ class SubscribeApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def subscribe
-    # @list_id = "cb644a7fb3"
-    # gb = Gibbon::API.new
+    @list_id = "cb644a7fb3"
+    gb = Gibbon::API.new
 
-    # gb.lists.subscribe({
-    #   :id => @list_id,
-    #   :email => {:email => params[:email][:address]}
-    # })
+    gb.lists.subscribe({
+      :id => @list_id,
+      :email => {:email => params[:email][:address]}
+    })
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: {"hi" => "bye"} }
       format.js
     end
   end
