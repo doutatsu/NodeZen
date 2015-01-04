12189b8b9e8ff57325f85a54c408f9c77d93d4d1
doutatsu <doutatsu@me.com>
2015-01-04 21:16:00 +0000

    trying to have AJAX subscribe

--- app/controllers/subscribe_api_controller.rb
+++ app/controllers/subscribe_api_controller.rb
@@ -1,14 +1,18 @@
 class SubscribeApiController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def subscribe
-    @list_id = "cb644a7fb3"
-    gb = Gibbon::API.new
 
-    gb.lists.subscribe({
-      :id => @list_id,
-      :email => {:email => params[:email][:address]}
-    })
-    
-    redirect_to '/'
+  def subscribe
+    # @list_id = "cb644a7fb3"
+    # gb = Gibbon::API.new
+
+    # gb.lists.subscribe({
+    #   :id => @list_id,
+    #   :email => {:email => params[:email][:address]}
+    # })
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: {"hi" => "bye"} }
+      format.js
+    end
   end
 end
