e7331171f3590a9e0b513d3a817b65f40efe4348
doutatsu <doutatsu@me.com>
2015-01-05 21:06:56 +0000

    trying to push to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ NodeZen::Application.routes.draw do
 
   root 'static_pages#home'
 
- if Rails.env.development?
+ # if Rails.env.development?
   resources :node, defaults: {format: :json}, only: [:index, :show]
 
   get '*path' => 'application#index'
- end
+ # end
 end
